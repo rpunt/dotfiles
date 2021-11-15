db8dfe2bc55429ce01f4ac4c3bf6b5d9ceda9c3c
Ryan Punt <ryan@mirum.org>
2021-11-15 12:58:56 -0600

    Update aliases.sh

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,5 +1,16 @@
 alias ll="ls -l"
 alias be="bundle exec"
-alias kls="bundle exec kitchen list"
-alias krm="bundle exec kitchen destroy"
-alias kmk="bundle exec kitchen converge"
+alias kls="be kitchen list"
+alias krm="be kitchen destroy"
+alias kmk="be kitchen converge"
+alias kv="be kitchen verify"
+
+bind "set completion-ignore-case on"
+bind "set completion-map-case on"
+bind "set show-all-if-ambiguous on"
+
+set -o vi
+
+# shopt -s autocd
+shopt -s cdspell
+shopt -s cdable_vars
