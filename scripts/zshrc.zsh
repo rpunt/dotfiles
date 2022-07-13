d912257f291316f7d3667276e315136e97f2b82a
Ryan Punt <ryan.punt@bestbuy.com>
2022-07-12 20:57:08 -0500

    add aws completions

--- scripts/zshrc.zsh
+++ scripts/zshrc.zsh
@@ -1,8 +1,12 @@
+autoload bashcompinit && bashcompinit
 # case-insensitive completions
 autoload -Uz compinit && compinit
+
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 
 # 1password CLI completions
 eval "$(op completion zsh)"; compdef _op op
+
+complete -C '/opt/homebrew/bin/aws_completer' aws
 
 set -o vi
