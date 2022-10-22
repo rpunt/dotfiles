ef4b8232fa79062aa1ba16bce4aee689202ae8b7
Ryan Punt <ryan@mirum.org>
2022-10-22 12:23:03 -0500

    load homebrew zsh completions

--- scripts/zshrc.zsh
+++ scripts/zshrc.zsh
@@ -1,4 +1,5 @@
 autoload bashcompinit && bashcompinit
+
 # case-insensitive completions
 autoload -Uz compinit && compinit
 
@@ -8,5 +9,10 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 eval "$(op completion zsh)"; compdef _op op
 
 [[ -s '/opt/homebrew/bin/aws_completer' ]] && complete -C '/opt/homebrew/bin/aws_completer' aws
+
+# load completions from Homebrew
+if type brew &>/dev/null; then
+  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+fi
 
 set -o vi
