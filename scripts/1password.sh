9398b250dc61f70e3a2d80c506a6aaec176e56a0
Ryan Punt <ryan.punt@bestbuy.com>
2023-12-13 09:54:54 -0600

    revert oplogin changes

--- scripts/1password.sh
+++ scripts/1password.sh
@@ -1,12 +1,10 @@
 # https://austincloud.guru/2018/11/27/1password-cli-tricks/
 
 oplogin() {
-  # see if we are logged in, will return exit code > 0 if not
-  op whoami 1>/dev/null
-
-  # if we are logged in, skip; if not, ask for master password
-  if [[ $? != 0 ]]; then
-    eval $(op signin)
+  op account get 1>/dev/null 2>&1
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    eval "$(op signin --account puntfamily)"
   fi
 }
 
