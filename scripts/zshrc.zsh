33d7b65629816aae9522c7ecf11245e7a6353e61
Ryan Punt <ryan@mirum.org>
2024-02-04 11:43:59 -0600

    add 1password ssh-agent support

--- scripts/zshrc.zsh
+++ scripts/zshrc.zsh
@@ -15,4 +15,10 @@ if type brew &>/dev/null; then
   FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 fi
 
+# if os = MacOS
+export SSH_AUTH_SOCK="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+# don't forget these also:
+# launchctl stop com.openssh.ssh-agent
+# launchctl disable com.openssh.ssh-agent
+
 set -o vi
