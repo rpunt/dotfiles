6b1689c5cf2b17c0ac2390874598785f9deeabc8
Ryan Punt <ryan@mirum.org>
2024-02-07 13:44:40 -0600

    add alias for cr_start_single

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -9,6 +9,8 @@ alias dbc='osascript $HOME/dev/dotfiles/scripts/setDefaultBrowser.scpt chrome'
 alias dbf='osascript $HOME/dev/dotfiles/scripts/setDefaultBrowser.scpt firefox'
 alias dbs='osascript $HOME/dev/dotfiles/scripts/setDefaultBrowser.scpt safari'
 
+alias cr_start_single='cockroach start-single-node --store=/opt/homebrew/var/cockroach --http-port=26256 --insecure --host=localhost'
+
 # still in beta
 # alias copilot='gh copilot'
 # alias cps='gh copilot suggest'
