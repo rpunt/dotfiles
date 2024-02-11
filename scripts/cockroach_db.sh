6922b28cad122c261aca24cc4d35874a55f3ee22
Ryan Punt <ryan@mirum.org>
2024-02-11 12:59:34 -0600

    simplify start and stop

--- scripts/cockroach_db.sh
+++ scripts/cockroach_db.sh
@@ -1,26 +1,58 @@
 alias cr_start_single='cockroach start-single-node --store=$(brew --prefix)/var/cockroach/single --http-port=26256 --insecure --host=localhost'
 
-function cr_start_n1() {
-  cockroach start --insecure --store="$(brew --prefix)/var/cockroach/node1" --listen-addr=localhost:26257 --http-addr=localhost:8080 --join=localhost:26257,localhost:26258,localhost:26259 --background --pid-file "$(brew --prefix)/var/run/cr_node1.pid"
+function cr_start_node() {
+  node='{
+    "1": {
+      "listen_addr": 26257,
+      "http_addr": 8080
+    },
+    "2": {
+      "listen_addr": 26258,
+      "http_addr": 8081
+    },
+    "3": {
+      "listen_addr": 26259,
+      "http_addr": 8082
+    }
+  }'
+
+  if [ -s "$(brew --prefix)/var/run/cr_node${1}.pid" ]; then
+    echo "PIDfile exists; is this node already running?"
+    return 1
+  fi
+
+  listen_addr=$(echo $node | jq --arg nodenumber "$1" '. | .[$nodenumber] | .listen_addr')
+  http_addr=$(echo $node | jq --arg nodenumber "$1" '. | .[$nodenumber] | .http_addr')
+  cockroach start \
+    --insecure \
+    --store="$(brew --prefix)/var/cockroach/node${1}" \
+    --listen-addr=localhost:${listen_addr} \
+    --http-addr=localhost:${http_addr} \
+    --join=localhost:26257,localhost:26258,localhost:26259 \
+    --background \
+    --pid-file "$(brew --prefix)/var/run/cr_node${1}.pid"
 }
-function cr_start_n2() {
-  cockroach start --insecure --store="$(brew --prefix)/var/cockroach/node2" --listen-addr=localhost:26258 --http-addr=localhost:8081 --join=localhost:26257,localhost:26258,localhost:26259 --background --pid-file "$(brew --prefix)/var/run/cr_node2.pid"
+function cr_stop_node() {
+  PIDFILE="$(brew --prefix)/var/run/cr_node${1}.pid"
+  kill -TERM "$(cat "$PIDFILE")" && rm -f "$PIDFILE"
 }
-function cr_start_n3() {
-  cockroach start --insecure --store="$(brew --prefix)/var/cockroach/node3" --listen-addr=localhost:26259 --http-addr=localhost:8082 --join=localhost:26257,localhost:26258,localhost:26259 --background --pid-file "$(brew --prefix)/var/run/cr_node3.pid"
+function cr_restart_node() {
+  echo "Stopping node ${1}"
+  cr_stop_node "$1"
+  sleep 5
+  echo "Starting node ${1}"
+  cr_start_node "$1"
 }
 
 function cr_start_cluster() {
-  cr_start_n1
-  cr_start_n2
-  cr_start_n3
   # for a new cluster, don't forget: cockroach init --insecure --host=localhost:26257
+  cr_start_node 1
+  cr_start_node 2
+  cr_start_node 3
 }
 
 function cr_stop_cluster() {
-  for pidfile in $(ls `brew --prefix`/var/run/cr_node*.pid 2>/dev/null); do
-    echo "$pidfile"
-    kill -TERM $(cat "$pidfile")
-    rm -f "$pidfile"
-  done
+  cr_stop_node 1
+  cr_stop_node 2
+  cr_stop_node 3
 }
