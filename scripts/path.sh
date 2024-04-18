1bb5bd294b059bd723cb9ed75ac3f2383a73a615
Ryan Punt <ryan.punt@cockroachlabs.com>
2024-04-18 09:45:35 -0500

    add to path: asdf python bins, cockroach bins

--- scripts/path.sh
+++ scripts/path.sh
@@ -3,3 +3,9 @@ export PATH="$PATH:$HOME/bin"
 
 # add gobins to PATH
 export PATH="$PATH:$HOME/go/bin"
+
+# asdf python bins
+export PATH="$PATH:$HOME/.local/bin"
+
+# work-internal: ensure tooling available
+export PATH="$PATH:$HOME/go/src/github.com/cockroachdb/cockroach/bin"
