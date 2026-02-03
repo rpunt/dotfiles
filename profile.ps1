fb2e294dc3196633e8f56a763f1a28b76844ddbe
Ryan Punt <ryan.punt@cockroachlabs.com>
2026-02-03 08:57:05 -0600

    conditional execution for oh-my-posh

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,6 @@
 Set-PSReadlineKeyHandler -Key ctrl+d -Function DeleteCharOrExit
 
-$scripts = Get-ChildItem "${Env:HOMEDRIVE}${Env:HOMEPATH}/dev/dotfiles/scripts/" -Filter "*.ps1"
+$scripts = Get-ChildItem "$HOME/dev/dotfiles/scripts/" -Filter "*.ps1"
 foreach ($script in $scripts) {
   . $script.FullName
 }
