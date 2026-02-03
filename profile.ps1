9500cb59b50d3e2959b384ad39324b39749d5a26
Ryan Punt <ryan.punt@cockroachlabs.com>
2026-02-03 08:50:34 -0600

    add CTRL-d to exit for powershell

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,5 @@
+Set-PSReadlineKeyHandler -Key ctrl+d -Function DeleteCharOrExit
+
 $scripts = Get-ChildItem "${Env:HOMEDRIVE}${Env:HOMEPATH}/dev/dotfiles/scripts/" -Filter "*.ps1"
 foreach ($script in $scripts) {
   . $script.FullName
