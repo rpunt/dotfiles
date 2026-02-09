73f42f288fdf60c386e78f6043c02aedf45e5f03
Ryan Punt <ryan.punt@cockroachlabs.com>
2026-02-09 15:40:45 -0600

    add ado functions

--- scripts/misc.ps1
+++ scripts/misc.ps1
@@ -8,3 +8,19 @@ function mktemp {
   }
   return Microsoft.PowerShell.Management\Join-Path ([System.IO.Path]::GetTempPath()) "$randomfile"
 }
+
+function prlist {
+  $(az repos pr list) | ConvertFrom-Json | Select-Object @{Name='ID'; Expression={$_.pullRequestId}}, @{Name='Title'; Expression={$_.title}} | Format-Table
+}
+
+function prbrowse {
+  $currentBranch = git branch --show-current
+  $prs = $(az repos pr list --source-branch $currentBranch) | ConvertFrom-Json
+  if ($prs.Count -gt 0) {
+    $pr = $prs[0]
+    az repos pr show --id $pr.pullRequestId --open | Out-Null
+  }
+  else {
+    Write-Host "No PR found for branch: $currentBranch"
+  }
+}
