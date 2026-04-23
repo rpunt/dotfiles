3035b1b0ea1f3e8cd51a4d84ebe43bf89e7f9e7a
Ryan Punt <ryan.punt@cockroachlabs.com>
2026-04-23 13:22:44 -0500

    update ado functions

---
+++ scripts/ado.ps1
@@ -0,0 +1,19 @@
+function azpr {
+  az repos pr create --auto-complete --delete-source-branch @args
+}
+
+function prlist {
+  $(az repos pr list) | ConvertFrom-Json | Select-Object @{Name = 'ID'; Expression = { $_.pullRequestId } }, @{Name = 'Title'; Expression = { $_.title } } | Format-Table
+}
+
+function prbrowse {
+  $currentBranch = git branch --show-current
+  $prs = $(az repos pr list --source-branch $currentBranch) | ConvertFrom-Json
+  if ($prs.Count -gt 0) {
+    $pr = $prs[0]
+    az repos pr show --id $pr.pullRequestId --open | Out-Null
+  }
+  else {
+    Write-Host "No PR found for branch: $currentBranch"
+  }
+}
