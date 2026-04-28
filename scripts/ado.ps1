29332251d81049e122838f75f85472e5589ac665
Ryan Punt <ryan.punt@cockroachlabs.com>
2026-04-28 08:52:54 -0500

    copy AZDO PR URL to clipboard

--- scripts/ado.ps1
+++ scripts/ado.ps1
@@ -1,5 +1,10 @@
 function azpr {
-  az repos pr create --auto-complete --delete-source-branch @args
+  $pr = az repos pr create --auto-complete --delete-source-branch @args | ConvertFrom-Json
+  $url = "$($pr.repository.webUrl)/pullrequest/$($pr.pullRequestId)"
+
+  $url | Set-Clipboard
+  Write-Host "PR URL copied to clipboard:"
+  $url
 }
 
 function prlist {
